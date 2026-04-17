c016355e3009f8d6f7b11c98e925ea98d6995bf4
unknown <jose.ochoa@gba.gob.ar>
2026-04-17 16:39:19 -0300

    corrijo estilos u UI

--- js/ui.js
+++ js/ui.js
@@ -4,7 +4,22 @@ export const actualizarContador = (carrito) => {
         contador.textContent = carrito.length
     }
 }
-
+/* 
 export const mostrarMensaje = (texto) => {
     alert(texto)
-}
+} */
+
+
+    export const mostrarMensaje = (texto) => {
+    // Creamos el elemento del mensaje
+    const toast = document.createElement("div");
+    toast.classList.add("toast-notificacion");
+    toast.textContent = texto;
+
+    document.body.appendChild(toast);
+
+    // Configuramos para que se elimine solo después de 3 segundos (3000ms)
+    setTimeout(() => {
+        toast.remove();
+    }, 3000);
+};
